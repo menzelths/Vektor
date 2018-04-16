655b282fa1670be2fb24b2b40fd883ab56ad0665
men <men@IR1-PC-01.musterschule.schule.paedml>
2018-04-16 17:15:59 +0200

    Final version

--- src/vektor/Main.java
+++ src/vektor/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Vektor v1=new Vektor(1,0,0);
+        Vektor v2=new Vektor(0,1,0);
         
+        System.out.println(v1.kreuzProdukt(v2).normiert());
     }
     
 }
--- src/vektor/Vektor.java
+++ src/vektor/Vektor.java
@@ -9,6 +9,54 @@ package vektor;
  *
  * @author men
  */
-public class Vektor {
+public class Vektor implements Addierbar<Vektor>{
+    private double v1,v2,v3;
     
+    public Vektor(double v1,double v2, double v3){
+        this.v1=v1;
+        this.v2=v2;
+        this.v3=v3;
+    }
+
+    @Override
+    public Vektor add(Vektor t) {
+        return new Vektor(t.v1+v1,t.v2+v2, t.v3+v3);
+    }
+    
+    public Vektor minus(Vektor t) {
+        return new Vektor(-t.v1+v1,-t.v2+v2, -t.v3+v3);
+    }
+    
+    public double skalar(Vektor t){
+        return t.v1*v1+t.v2*v2+t.v3*v3;
+    }
+    
+    public Vektor multipliziereMitDouble(double z){
+        return new Vektor(v1*z,v2*z,v3*z);
+    }
+    
+    public double betrag(){
+        return Math.sqrt(this.skalar(this));
+    }
+    
+    public Vektor kreuzProdukt(Vektor t){
+        return new Vektor(t.v2*v3-t.v3*v2,t.v3*v1-t.v1*v3,t.v1*v2-t.v2*v1);
+    }
+    
+    public Vektor normiert(){
+        return multipliziereMitDouble(1/betrag());
+    }
+            
+    
+    
+            
+            
+            
+            
+            
+    
+    @Override
+    public String toString(){
+        return "["+v1+","+v2+","+v3+"]";
+    }
 }
